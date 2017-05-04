4e858fb87dad6f3db2bd8a8e6c127a9cd387274b
Bhaskar Srinivas Gurram <bsg348@login-1-1.local>
2017-05-04 12:09:58 -0400

    adding the final code for PickupCount.py

--- PickupCount.py
+++ PickupCount.py
@@ -7,40 +7,55 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import split
 from pyspark.sql.functions import col
 
+#creating the spark context
 sc = SparkContext.getOrCreate()
+
+#creating the SQLContext
 sqlContext = SQLContext(sc)
-data1 = sc.textFile("citibikedata/201601-citibike-tripdata.csv")
-data2 = sc.textFile("citibikedata/201611-citibike-tripdata.csv")
-data=sc.textFile("citibikedata")
-header = data.first()
+
+#the header of November and December month differs from the rest of the month, sol loading Decembers data and getting
+data2 = sc.textFile("citibikedata/201612-citibike-tripdata.csv")
 header2 = data2.first()
+
+#reading the complete citibike data
+data=sc.textFile("citibikedata")
+#taking its header
+header = data.first()
+
+#removing the header rows from the data
 data = data.filter(lambda row : row != header and row != header2)
 data = data.map(lambda row : row.replace('"',''))
+
+#splitting the raw "," separated data into appropriate format
 data = data.map(lambda x: x.split(",")).\
     map(lambda y: (y[1],float(y[5]),float(y[6])))
-#
 
+#creating the schema for dataframe
 customSchema = StructType([ \
     StructField("starttime", StringType(), True), \
     StructField("start_station_latitude", DoubleType(), True), \
     StructField("start_station_longitude", DoubleType(), True)])
 
+#creating the dataframe
 df = sqlContext.createDataFrame(data, customSchema)
 split_col = split(df['starttime'], ' ')
 df.show()
+
+
+#the start time is in MM/DD HH:MM format, extracting the date and time from this data
+df = sqlContext.createDataFrame(data, customSchema)
+split_col = split(df['starttime'], ' ')
 df = df.withColumn('start_date', split_col.getItem(0))
 df = df.withColumn('start_t', split_col.getItem(1))
-
-
 df.show()
+
+#the date is in format MM/DD, extracting the date and month from this
 split_col1 = split(df['start_date'], "/")
 df = df.withColumn('start_month', split_col1.getItem(0))
 df = df.withColumn('start_d', split_col1.getItem(1))
-
-#df = df.withColumn('start_year', split_col.getItem(2))
-
-#df=df.drop('start_date').collect()
 df.show()
+
+#the time is in HH:MM format, extracting the hour and minute information from this
 split_col2 = split(df['start_t'], ':')
 df = df.withColumn('start_hour', split_col2.getItem(0))
 df = df.withColumn('start_minute', split_col2.getItem(1))
