0056399bc689a93a52d5439286a4241c36904574
Bhaskar Srinivas Gurram <bsg348@login-1-1.local>
2017-05-04 12:21:22 -0400

    Final Dropoff code

--- DropoffCount.py
+++ DropoffCount.py
@@ -7,31 +7,40 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import split
 from pyspark.sql.functions import col
 
+#creating Spark Context
 sc = SparkContext.getOrCreate()
+
+#creating SQL Context
 sqlContext = SQLContext(sc)
-data1 = sc.textFile("citibikedata/201601-citibike-tripdata.csv")
-data2 = sc.textFile("citibikedata/201611-citibike-tripdata.csv")
+
+#loading the entire citibike data which contains individual monthwise data
 data=sc.textFile("citibikedata")
 header = data.first()
+
+#the header names of November and december data differs, so taking into account their names as well
+data2 = sc.textFile("citibikedata/201611-citibike-tripdata.csv")
 header2 = data2.first()
+
+#removing the header from the data
 data = data.filter(lambda row : row != header and row != header2)
 data = data.map(lambda row : row.replace('"',''))
-#data=data.filter(lambda row:row.replaceAll('\"',''))
+
+#splitting the raw data which are "," separated
 data = data.map(lambda x: x.split(",")).\
     map(lambda y: (float(y[9]),float(y[10])))
-#
 
+#defining the schema
 customSchema = StructType([ \
     StructField("end_station_latitude", DoubleType(), True), \
     StructField("end_station_longitude", DoubleType(), True)])
 
+#the start time is in MM/DD HH:MM format, extracting the date and time from this data
 df = sqlContext.createDataFrame(data, customSchema)
 split_col = split(df['starttime'], ' ')
-df.show()
 df = df.withColumn('start_date', split_col.getItem(0))
 df = df.withColumn('start_t', split_col.getItem(1))
 df.show()
 
-
+#grouping on the basis of endstation
 groupby_station_count=df.groupby(['end_station_latitude','end_station_longitude']).count()
 groupby_station_count.toPandas().to_csv('dropoff_count.csv')
